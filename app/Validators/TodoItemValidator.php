a9e2b119ce229b6ec9fc7e6d7f663fb406a86b9e
yusuf_taufiqurrahman <yusuf_taufiqurrahman@quick.com>
2022-01-03 20:08:44 +0700

    Add todo item validation

---
+++ app/Validators/TodoItemValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+final class TodoItemValidator
+{
+    final public static function validateStore(array $data): bool|string
+    {
+        if (
+            array_key_exists('activity_group_id', $data) === false
+            || is_int($data['activity_group_id']) === false
+        ) {
+            return 'activity_group_id cannot be null';
+        }
+
+        if (
+            array_key_exists('title', $data) === false
+            || is_int($data['title']) === false
+        ) {
+            return 'title cannot be null';
+        }
+
+        return true;
+    }
+}
