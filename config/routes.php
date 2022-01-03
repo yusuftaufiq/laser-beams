e1e24efe2df5032d20128571423a4853ddb52a41
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-01-03 11:30:19 +0700

    Add type hinting

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,5 @@
 <?php
 
-declare(strict_types=1);
 /**
  * This file is part of Simps.
  *
@@ -8,10 +7,17 @@ declare(strict_types=1);
  * @document https://doc.simps.io
  * @license  https://github.com/simple-swoole/simps/blob/master/LICENSE
  */
+
+declare(strict_types=1);
+
+use App\Controller\IndexController;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+
 return [
-    ['GET', '/', '\App\Controller\IndexController@index'],
-    ['GET', '/hello[/{name}]', '\App\Controller\IndexController@hello'],
-    ['GET', '/favicon.ico', function ($request, $response) {
-        $response->end('');
+    ['GET', '/', IndexController::class . '@' . 'index'],
+    ['GET', '/hello[/{name}]', IndexController::class  . '@' . 'hello'],
+    ['GET', '/favicon.ico', function (Request $request, Response $response) {
+        $response->end();
     }],
 ];
