cd7a56f7416f1b85d2871679574b4247aa99381a
yusuf_taufiqurrahman <yusuf_taufiqurrahman@quick.com>
2022-01-03 20:11:27 +0700

    Initial commit todo item controller

---
+++ app/Controllers/TodoItemController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of Simps.
+ *
+ * @link     https://simps.io
+ * @document https://doc.simps.io
+ * @license  https://github.com/simple-swoole/simps/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+
+final class TodoItemController
+{
+    final public function index(Request $request, Response $response): void
+    {
+        # code...
+    }
+}
