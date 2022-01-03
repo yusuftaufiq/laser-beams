aa8e7f21bb01397ee51a4c10ec5a9cfd6372ea7a
yusuf_taufiqurrahman <yusuf_taufiqurrahman@quick.com>
2022-01-03 20:13:44 +0700

    Add show a todo feature

--- app/Controllers/TodoItemController.php
+++ app/Controllers/TodoItemController.php
@@ -12,11 +12,14 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Model\TodoItem;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 
 final class TodoItemController
 {
+    final public const NOT_FOUND_MESSAGE = 'Todo with ID %d Not Found';
+
     final public function index(Request $request, Response $response): void
     {
         $todo = new TodoItem();
@@ -30,6 +33,20 @@ final class TodoItemController
 
         $response->setStatusCode(ResponseHelper::HTTP_OK);
         $response->end(ResponseHelper::success($items));
-        # code...
+    }
+
+    final public function show(Request $request, Response $response, int $id): void
+    {
+        $todo = new TodoItem();
+
+        $item = $todo->find($id);
+
+        if ($item === false) {
+            $response->setStatusCode(ResponseHelper::HTTP_NOT_FOUND);
+            $response->end(ResponseHelper::notFound(sprintf(self::NOT_FOUND_MESSAGE, $id)));
+        }
+
+        $response->setStatusCode(ResponseHelper::HTTP_OK);
+        $response->end(ResponseHelper::success($item));
     }
 }
