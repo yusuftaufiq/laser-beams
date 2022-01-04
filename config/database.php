5872447283430b40ce83389bca94e64a8ba8ac23
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-01-04 10:15:12 +0700

    Use environment variables

--- config/database.php
+++ config/database.php
@@ -11,11 +11,11 @@
 declare(strict_types=1);
 
 return [
-    'host' => 'localhost',
+    'host' => getenv('MYSQL_HOST'),
     'port' => 3306,
-    'database' => 'test',
-    'username' => 'root',
-    'password' => 'root',
+    'database' => getenv('MYSQL_DBNAME'),
+    'username' => getenv('MYSQL_USER'),
+    'password' => getenv('MYSQL_PASSWORD'),
     'charset' => 'utf8mb4',
     'options' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
