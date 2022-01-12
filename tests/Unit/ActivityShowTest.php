a4f0d409b8c37986ce044ee2443b7c1d594f3471
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-01-12 14:30:14 +0700

    Add unit test show one activity

---
+++ tests/Unit/ActivityShowTest.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Controllers;
+
+use App\Controllers\ActivityController;
+use App\Repositories\ActivityRepositoryInterface;
+use PHPUnit\Framework\TestCase;
+use Tests\ActivityTrait;
+use Tests\FakerTrait;
+
+/**
+ * @method \PHPUnit\Framework\MockObject\MockObject createMock
+ */
+class ActivityShowTest extends TestCase
+{
+    use ActivityTrait;
+    use FakerTrait;
+
+    public function setUp(): void
+    {
+        $this->setUpFaker();
+    }
+
+    public function tearDown(): void
+    {
+        $this->tearDownFaker();
+    }
+
+    public function testShowActivity(): void
+    {
+        $expect = $this->generateActivity();
+        $id = $expect['id'];
+
+        $activityRepositoryMock = $this
+            ->getMockBuilder(ActivityRepositoryInterface::class)
+            ->onlyMethods(['find'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $activityRepositoryMock
+            ->expects($this->once())
+            ->method('find')
+            ->with($id)
+            ->willReturn($expect);
+
+        $requestMock = $this->createMock(\Swoole\Http\Request::class);
+
+        $responseMock = $this->createMock(\Swoole\Http\Response::class);
+        $responseMock
+            ->expects($this->exactly(1))
+            ->method('setHeader')
+            ->withConsecutive(['Content-Type', 'application/json']);
+        $responseMock
+            ->expects($this->once())
+            ->method('setStatusCode')
+            ->with(200);
+        $responseMock
+            ->expects($this->once())
+            ->method('end')
+            ->with(json_encode([
+                'status' => 'Success',
+                'message' => 'OK',
+                'data' => $expect,
+            ]));
+
+        /**
+         * @var \App\Repositories\ActivityRepositoryInterface $activityRepositoryMock
+         * @var \Swoole\Http\Request                          $requestMock
+         * @var \Swoole\Http\Response                         $responseMock
+         */
+        $activity = new ActivityController($activityRepositoryMock);
+        $activity->show($requestMock, $responseMock, ['id' => $id]);
+    }
+
+    public function testShowActivityNotFound(): void
+    {
+        $expect = (object) [];
+        $id = $this->generateActivityId();
+
+        $activityRepositoryMock = $this
+            ->getMockBuilder(ActivityRepositoryInterface::class)
+            ->onlyMethods(['find'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $activityRepositoryMock
+            ->expects($this->once())
+            ->method('find')
+            ->with($id)
+            ->willReturn(null);
+
+        $requestMock = $this->createMock(\Swoole\Http\Request::class);
+
+        $responseMock = $this->createMock(\Swoole\Http\Response::class);
+        $responseMock
+            ->expects($this->exactly(1))
+            ->method('setHeader')
+            ->withConsecutive(['Content-Type', 'application/json']);
+        $responseMock
+            ->expects($this->once())
+            ->method('setStatusCode')
+            ->with(404);
+        $responseMock
+            ->expects($this->once())
+            ->method('end')
+            ->with(json_encode([
+                'status' => 'Not Found',
+                'message' => $this->generateNotFoundMessage($id),
+                'data' => $expect,
+            ]));
+
+        /**
+         * @var \App\Repositories\ActivityRepositoryInterface $activityRepositoryMock
+         * @var \Swoole\Http\Request                          $requestMock
+         * @var \Swoole\Http\Response                         $responseMock
+         */
+        $activity = new ActivityController($activityRepositoryMock);
+        $activity->show($requestMock, $responseMock, ['id' => $id]);
+    }
+}
