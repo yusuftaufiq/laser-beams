68a4033188d74ad891a7bae106c59198b7b757fd
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-01-15 06:15:16 +0700

    Use Symfony Validators component

--- app/Validators/ActivityValidator.php
+++ app/Validators/ActivityValidator.php
@@ -4,12 +4,63 @@ declare(strict_types=1);
 
 namespace App\Validators;
 
+use App\Helpers\SelfInstantiateHelper;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 final class ActivityValidator
 {
-    final public static function validateStore(array $data): ?string
+    use SelfInstantiateHelper {
+        SelfInstantiateHelper::instantiate as validate;
+    }
+
+    private ValidatorInterface $validator;
+
+    final public function __construct()
     {
-        if (array_key_exists('title', $data) === false) {
-            return 'title cannot be null';
+        $this->validator = Validation::createValidator();
+    }
+
+    final public function store(array $input): ?ConstraintViolationListInterface
+    {
+        $constraint = new Assert\Collection([
+            'title' => new Assert\Required([
+                new Assert\NotBlank(),
+                new Assert\NotNull(),
+            ]),
+            'email' => new Assert\Optional([
+                new Assert\Email(),
+            ]),
+        ]);
+
+        $violations = $this->validator->validate($input, $constraint);
+
+        if ($violations->count() > 0) {
+            return new ConstraintViolationList($violations);
+        }
+
+        return null;
+    }
+
+    final public function update(array $input): ?ConstraintViolationListInterface
+    {
+        $constraint = new Assert\Collection([
+            'title' => new Assert\Optional([
+                new Assert\NotBlank(),
+                new Assert\NotNull(),
+            ]),
+            'email' => new Assert\Optional([
+                new Assert\Email(),
+            ]),
+        ]);
+
+        $violations = $this->validator->validate($input, $constraint);
+
+        if ($violations->count() > 0) {
+            return new ConstraintViolationList($violations);
         }
 
         return null;
--- app/Validators/TodoItemValidator.php
+++ app/Validators/TodoItemValidator.php
@@ -4,16 +4,74 @@ declare(strict_types=1);
 
 namespace App\Validators;
 
+use App\Helpers\SelfInstantiateHelper;
+use App\Repositories\DB\TodoItemRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 final class TodoItemValidator
 {
-    final public static function validateStore(array $data): ?string
+    use SelfInstantiateHelper {
+        SelfInstantiateHelper::instantiate as validate;
+    }
+
+    final public function __construct(
+        private ValidatorInterface $validator = Validation::createValidator()
+    ) {
+    }
+
+    final public function store(array $input): ?ConstraintViolationListInterface
     {
-        if (array_key_exists('activity_group_id', $data) === false) {
-            return 'activity_group_id cannot be null';
+        $constraint = new Assert\Collection([
+            'activity_group_id' => new Assert\Required([
+                new Assert\Positive(),
+            ]),
+            'title' => new Assert\Required([
+                new Assert\NotBlank(),
+                new Assert\NotNull(),
+            ]),
+            'is_active' => new Assert\Optional([
+                new Assert\Choice([true, false]),
+            ]),
+            'priority' => new Assert\Optional([
+                new Assert\Choice(TodoItemRepository::PRIORITY),
+            ]),
+        ]);
+
+        $violations = $this->validator->validate($input, $constraint);
+
+        if ($violations->count() > 0) {
+            return new ConstraintViolationList($violations);
         }
 
-        if (array_key_exists('title', $data) === false) {
-            return 'title cannot be null';
+        return null;
+    }
+
+    final public function update(array $input): ?ConstraintViolationListInterface
+    {
+        $constraint = new Assert\Collection([
+            'activity_group_id' => new Assert\Optional([
+                new Assert\Positive(),
+            ]),
+            'title' => new Assert\Optional([
+                new Assert\NotBlank(),
+                new Assert\NotNull(),
+            ]),
+            'is_active' => new Assert\Optional([
+                new Assert\Choice([true, false]),
+            ]),
+            'priority' => new Assert\Optional([
+                new Assert\Choice(TodoItemRepository::PRIORITY),
+            ]),
+        ]);
+
+        $violations = $this->validator->validate($input, $constraint);
+
+        if ($violations->count() > 0) {
+            return new ConstraintViolationList($violations);
         }
 
         return null;
